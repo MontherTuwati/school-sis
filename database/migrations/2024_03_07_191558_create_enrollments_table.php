b928ad9587468581dc3cde78d196ff04963caae6
Monther Tuwati <monthertuwati@gmail.com>
2024-11-22 10:00:00 -0500

    Implement migration tables

---
+++ database/migrations/2024_03_07_191558_create_enrollments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->string('course_code');
+            $table->unsignedInteger('status_id')->default(1);
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('restrict');
+            $table->foreign('course_code')->references('course_code')->on('courses')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
