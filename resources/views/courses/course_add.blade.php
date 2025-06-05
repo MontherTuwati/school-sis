02ec95686494c43030626d9eadd42e5ee3ef182f
Monther Tuwati <monthertuwati@gmail.com>
2025-06-05 16:49:00 -0400

    Add Communication, Timetable, and Examination Management Controllers with comprehensive course, subject, event, and graduate views for complete academic management system

---
+++ resources/views/courses/course_add.blade.php
@@ -0,0 +1,56 @@
+
+@extends('layouts.master')
+@section('content')
+    {{-- message --}}
+    {!! Toastr::message() !!}
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="page-title">Add Course</h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="courses.html">Course</a></li>
+                            <li class="breadcrumb-item active">Add Course</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('course/save') }}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h5 class="form-title"><span>Course Information</span></h5>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label> Course Name <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control" name="title" placeholder="Enter Course Name">
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label> Description <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control" name="description" placeholder="Enter Description">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="student-submit">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+@endsection
